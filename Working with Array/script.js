eb615a6681686a52be58a4ef1a2a4d7380da8c28
Your Name <sahil.savaliya@simformsolutions.com>
2023-03-22 17:52:58 +0530

    working with Arrays

---
+++ Working with Array/script.js
@@ -0,0 +1,221 @@
+/* eslint-disable no-console */
+
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Simple Array Methods
+// SLICE
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+
+// console.log(arr.splice(2));
+
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+
+console.log(letters.join(' _ '));
+
+// The new at Method
+
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+
+// getting last array element
+
+console.log(arr3[arr3.length - 1]);
+console.log(arr3.slice(-1)[0]);
+console.log(arr3.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
+
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log('---- FOREACH ----');
+
+movements.forEach((mov, i) => {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// forEach With Maps and Sets
+// Map
+
+const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+  ]);
+
+  currencies.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
+
+  // Set
+
+  const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+  console.log(currenciesUnique);
+
+  currenciesUnique.forEach((value, key) => {
+    console.log(`${value}: ${key}`);
+  });
+
+  // The map Method
+  
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
+
+// The filter Method
+const deposits = movements.filter((mov) => 
+   mov > 0);
+console.log(movements);
+console.log(deposits);
+
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+// The reduce Method
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+
+const balance = movements.reduce((acc, cur, i) => {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+
+console.log(balance);
+
+const balance1 = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance1);
+
+// Maximum value
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) 
+    return acc;
+  return mov;
+}, movements[0]);
+
+console.log(max);
+
+// The Magic of Chaining Methods
+
+const eurToUsd1 = 1.1;
+console.log(movements);
+
+// PIPELINE
+
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov) => mov * eurToUsd1)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+// The find Method
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+// some and every
+
+console.log(movements);
+
+// EQUALITY
+
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+
+console.log(movements.some(mov => mov === -130));
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
