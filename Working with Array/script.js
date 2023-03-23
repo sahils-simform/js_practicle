c4e47fd560ce9d27837e85258b5a806a4428187a
Your Name <sahil.savaliya@simformsolutions.com>
2023-03-23 17:51:59 +0530

    working with array-2

--- Working with Array/script.js
+++ Working with Array/script.js
@@ -30,6 +30,9 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+
+const labelBalance = document.querySelector('.balance__value');
+
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // Simple Array Methods
@@ -219,3 +222,100 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+// Sorting Arrays
+
+// Strings
+
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+
+console.log(movements);
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+// More Ways of Creating and Filling Arrays
+
+const arr4 = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array([1, 2, 3, 4, 5, 6, ]));
+
+// Emprty arrays + fill method
+
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+arr.fill(23, 2, 6);
+console.log(arr4);
+
+// Array.from
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+labelBalance.addEventListener('click', () => {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+});
+
+// Array Methods Practice
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum)
+
+
+// 2.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = (title) => {
+  const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitzalize(word)))
+    .join(' ');
+
+  return capitzalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
